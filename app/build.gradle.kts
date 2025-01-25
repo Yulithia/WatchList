c357abfbd67bfa602d939bb2c335d717dc36192b
Yulithia <julita.250@wp.pl>
2025-01-25 16:46:36 +0100

    Added retrofit dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,12 @@
+import java.util.Properties
+
+val secretsFile = rootProject.file("secrets.properties")
+val secrets = Properties()
+
+if (secretsFile.exists()) {
+    secrets.load(secretsFile.inputStream())
+}
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -5,16 +14,18 @@ plugins {
 
 android {
     namespace = "com.example.watchlist"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.watchlist"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        buildConfigField("String", "TMDB_API_KEY", "\"${secrets["TMDB_API_KEY"]}\"")
     }
 
     buildTypes {
@@ -33,6 +44,8 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+
 }
 
 dependencies {
@@ -45,4 +58,12 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation (libs.androidx.lifecycle.viewmodel.compose)
+    implementation (libs.androidx.lifecycle.viewmodel.ktx)
+    implementation (libs.androidx.lifecycle.runtime.compose)
+
+    implementation (libs.retrofit)
+    implementation (libs.converter.gson)
+    implementation (libs.okhttp3.logging.interceptor)
 }
